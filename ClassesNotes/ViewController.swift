fd59d03ebe6458d8857d88aff254bea60abe9e7d
Ava Schmidt <avamschmidt07@gmail.com>
2024-09-30 08:15:26 -0500

    Initial Commit

---
+++ ClassesNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassesNotes
+//
+//  Created by AVA SCHMIDT on 9/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
